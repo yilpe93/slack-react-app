48dacab9f001110a31a5fb3335a2b9b722c91ba5
yilpe93 <yilpe@naver.com>
2021-05-23 23:57:00 +0900

    Add - Profile modal & menu

---
+++ front/components/Menu/index.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties, FC, PropsWithChildren, useCallback } from 'react';
+import { CreateMenu, CloseModalButton } from '@components/Menu/styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<PropsWithChildren<Props>> = ({ closeButton, style, show, children, onCloseModal }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div onClick={stopPropagation} style={style}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
